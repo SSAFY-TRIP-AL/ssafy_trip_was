c52747ee938abbdb1b90e9f1e2aa6f45e8ed9722
lgr0831 <qfrub0@gmail.com>
2026-06-04 13:19:51 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/test/java/com/ssafy/tripbaton/TripbatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.tripbaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripbatonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

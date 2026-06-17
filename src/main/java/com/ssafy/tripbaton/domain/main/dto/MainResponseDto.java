95f06adf072962190e47ea7f485a0dc1ae66e624
nyeonseok <aragorn2358@naver.com>
2026-06-17 16:21:45 +0900

    feat: Main도메인 dto 구현

---
+++ src/main/java/com/ssafy/tripbaton/domain/main/dto/MainResponseDto.java
@@ -0,0 +1,17 @@
+package com.ssafy.tripbaton.domain.main.dto;
+
+import com.ssafy.tripbaton.domain.relay.dto.RelayListItemDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class MainResponseDto {
+
+    private Long userCount;
+    private List<TopUserDto> topUsers;
+    private List<RelayListItemDto> relays;
+
+}

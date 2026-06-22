b7c3f3e60fa7bc62b85c6a5e895467793ad3721a
nyeonseok <aragorn2358@naver.com>
2026-06-22 09:33:44 +0900

    feat: RelayStatus 추가

--- src/main/java/com/ssafy/tripbaton/domain/relay/entity/RelayStatus.java
+++ src/main/java/com/ssafy/tripbaton/domain/relay/entity/RelayStatus.java
@@ -1,5 +1,17 @@
 package com.ssafy.tripbaton.domain.relay.entity;
 
 public enum RelayStatus {
-    ACTIVE, STALE, ARCHIVED
+    // 활성화 된 릴레이
+    ACTIVE,
+
+    STALE,
+
+    // 배치에 의해 닫힌 릴레이
+    ARCHIVED,
+
+    // 참여 가능한 릴레이
+    OPEN,
+
+    // 누군가 참여 중인 릴레이
+    RUNNING
 }
